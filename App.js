3e44d3bc14d997d9fbe02a2270a5b4863d44b27c
bojan.petrov <bojan.petrov@clarivate.com>
2019-10-23 16:44:23 +0200

    changes

--- App.js
+++ App.js
@@ -5,19 +5,8 @@ import Login from './src/views/login';
 import Root from './src/views/main';
 import GlobalStyles from './src/styles/styles';
 
-<<<<<<< HEAD
-const App = () => {
-  return (
-          <View style={{flex:1}}>
-              <Main/>
-            </View>
-   
-  );
-};
 
-=======
 class App extends React.Component {
->>>>>>> 25d63896c151960ba5fede5f752ed6cb7c4d308a
 
     render() {
     return (
